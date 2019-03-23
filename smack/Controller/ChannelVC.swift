15798240721aabde69345d98e1c969d7d00a36f6
Whaylon Coleman <it@anitconsultant.com>
2019-03-23 15:28:36 -0500

    Added "End of lesson 2"

---
+++ smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  smack
+//
+//  Created by Whaylon Coleman on 3/23/19.
+//  Copyright © 2019 AnITConsultant, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+    
+
+    
+
+}
---
+++ smack/Controller/ChatVC.swift
@@ -0,0 +1,38 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by Whaylon Coleman on 3/23/19.
+//  Copyright © 2019 AnITConsultant, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
