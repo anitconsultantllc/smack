18220bf980ae1419f8e85bf658ea2ed624eb1510
Whaylon Coleman <it@anitconsultant.com>
2019-04-01 00:31:45 -0500

    Got Login screen working

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Whaylon Coleman on 4/1/19.
+//  Copyright © 2019 AnITConsultant, LLC. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Whaylon Coleman on 3/31/19.
+//  Copyright © 2019 AnITConsultant, LLC. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
