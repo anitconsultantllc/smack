dc175c092a389c81c9d63b66d50524ac9f1858f8
Whaylon Coleman <it@anitconsultant.com>
2019-04-04 09:19:26 -0500

    Added avatar UI elements, segue, collectionview, etc. (#2)

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -12,14 +12,22 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 
 //URL Constants
 let BASE_URL = "https://smackchatapi2019.herokuapp.com/v1/"
+
+
 //let BASE_URL = "http://localhost:3005/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_ = "\(BASE_URL)"
 
+
+//Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+let TO_AVATAR_PICKER = "toAvatarPicker"
+
+
 //User Defaults
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
