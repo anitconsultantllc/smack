147beb66ef7e0233d8349f7518c51d053ea3c887
Whaylon Coleman <it@anitconsultant.com>
2019-04-05 14:43:58 -0500

    Got the modal window working that shows the user profile.

--- smack/Services/UserDataService.swift
+++ smack/Services/UserDataService.swift
@@ -67,5 +67,47 @@ class UserDataService{
             }
         }
     }
+    
+    func returnUIColor(components: String) -> UIColor {
+    //    "avatarColor": "[0.7725490196078432, 0.796078431372549, 0.39215686274509803, 1]"
+        let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a : NSString?
+        
+        scanner.scanUpToCharacters(from:comma, into: &r)
+        scanner.scanUpToCharacters(from:comma, into: &g)
+        scanner.scanUpToCharacters(from:comma, into: &b)
+        scanner.scanUpToCharacters(from:comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        guard let rUnwrapped = r else {return defaultColor }
+        guard let gUnwrapped = g else {return defaultColor }
+        guard let bUnwrapped = b else {return defaultColor }
+        guard let aUnwrapped = a else {return defaultColor }
+        
+        let rfloat = CGFloat(rUnwrapped.doubleValue)
+        let gfloat = CGFloat(gUnwrapped.doubleValue)
+        let bfloat = CGFloat(bUnwrapped.doubleValue)
+        let afloat = CGFloat(aUnwrapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rfloat, green: gfloat, blue: bfloat, alpha: afloat)
+        
+        return newUIColor
+    
+    }
+    
+    func logOutUser(){
+        id = ""
+        avatarName = ""
+        avatarColor = ""
+        email = ""
+        name = ""
+        AuthService.instance.isLoggedIn = false
+        AuthService.instance.userEmail = ""
+        AuthService.instance.authToken = ""
+    }
 }
 
