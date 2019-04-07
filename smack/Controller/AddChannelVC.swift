d4eede4b4504183e5c71ad6a3de88d9385ece231
Whaylon Coleman <it@anitconsultant.com>
2019-04-07 15:19:09 -0500

    Added Add Channel partial functionality

---
+++ smack/Controller/AddChannelVC.swift
@@ -0,0 +1,45 @@
+//
+//  AddChannelVC.swift
+//  smack
+//
+//  Created by Whaylon Coleman on 4/7/19.
+//  Copyright © 2019 AnITConsultant, LLC. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    //IBOutlets
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    //IBActions
+    @IBAction func createChannelPressed(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+
+    func setupView(){
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedString.Key.foregroundColor : smackPurpleColorPlaceHolders])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedString.Key.foregroundColor : smackPurpleColorPlaceHolders])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer){
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
